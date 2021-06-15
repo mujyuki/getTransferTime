c9a96315a82797b439cac499a311e163ac18d6da
mujyuki <52305141+mujyuki@users.noreply.github.com>
2021-06-16 00:53:22 +0900

    Add files via upload

--- get_ek.py
+++ get_ek.py
@@ -4,39 +4,36 @@ import csv
 import re
 
 pref_list = ['tokyo', 'kanagawa', 'chiba', 'saitama']
-line_list = ['en_keiosen', 'en_odakyusen']
+# line_list = ['en_keiosen', 'en_odakyusen'] #getEnsenNameにより不要
 
 def getEnsenName(pref):
-	patern = r'/chintai/' + pref + '/en_.*'
-	print(patern)
 	load_url = "https://suumo.jp/chintai/" + pref + "/ensen/"
 	html = requests.get(load_url)
 	soup = BeautifulSoup(html.content, "html.parser")
+	patern = r'/chintai/' + pref + '/en_.*' #正規表現作成
 	ensen_list = soup.find_all('a', attrs={ 'href': re.compile(patern) })
-	for ensen in ensen_list:
-		print(ensen['href'])
+	url_list = [ i['href'] for i in ensen_list ] #リスト内包表記
+	return url_list
 
 for pref in pref_list:
-	for line in line_list:
-		load_url = "https://suumo.jp/chintai/" + pref + "/" + line + "/"
+	url_list = getEnsenName(pref)
+	for target_url in url_list:
+		load_url = "https://suumo.jp/" + target_url
 		html = requests.get(load_url)
 		soup = BeautifulSoup(html.content, "html.parser")
-
+		print(load_url)
 		resultset = soup.find_all('input', attrs={ 'name': 'ek' })
 
 		# rn必要なし
 		# rn = soup.find('input', attrs={ 'name': 'rn' })
 		# print(rn['value'])
 
-		with open('ek_test.csv', 'a') as f:
+		with open('ek_test_all.csv', 'a') as f:
 			writer = csv.writer(f)
 			for result in resultset:
+				print(result)
 				ek = result['value']
 				station_name = result.next_element.find('span').string
 				writer.writerow([station_name, ek])
-
-if __name__ == '__main__':
-	for pref in pref_list:
-		getEnsenName(pref)
 
 
